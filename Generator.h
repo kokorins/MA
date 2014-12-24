59ce111e7d2e0730da10bd018a1358f67aec690e
kokorins <kokorins@yandex.ru>
2014-12-24 11:42:12 +0300

    Update Generator.h

--- Generator.h
+++ Generator.h
@@ -4,19 +4,24 @@
 #include "SRMap.h"
 #include "SetSRMaps.h"
 
+//TODO: wrap in struct v
 const int probMissed = 100;
 const int countCuts = 100;
 const int defCountCuts = 20;
 const int probError = 2;
 const int maxCountCuts = 10;
+//^
 
+/**
+ * Add consts to all functions if they dont modify code
+ */
 class Generator
 {
 public:
 	SRMap GenerateMap(int countCuts);
-	SetSRMaps * GenerateSetMap(int countMaps);
-	SetSRMaps * GenerateSetMap(char * filename);
-	bool isTrue(int probability);
+	SetSRMaps * GenerateSetMap(int countMaps); // why bare pointer?
+	SetSRMaps * GenerateSetMap(char * filename); // why bare pointer and bare non const pointer on arg?
+	bool isTrue(int probability); //TODO: < generalize function
 };
 
 #endif
